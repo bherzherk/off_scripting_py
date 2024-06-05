23708ccfc8fc6e6b6ae9a64d3fb3f73420766f47
bherzherk <bherzherk@gmail.com>
2024-06-04 23:53:20 -0400

    HTTPS image sniffer, using mitmdump

---
+++ image_sniffer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from mitmproxy import http
+
+def response(packet):
+    content_type = packet.response.headers.get("content-type", "")
+
+    try:
+        if "image" in content_type:
+            url = packet.request.url
+            extension = content_type.split("/")[-1]
+
+            if extension == "jpeg":
+                extension = "jpg"
+
+                file_name = f"../image_sniff/{url.replace('/', '_').replace(':', '_')}.{extension}"
+                image_data = packet.response.content
+
+                with open(file_name, "wb") as f:
+                    f.write(image_data)
+
+                print(f"[+] Image saved: {file_name}")
+    except:
+        pass
